28878d272514331bf551206985b2ab218555ce9e
afei <88603982@qq.com>
2016-04-24 10:30:05 +0800

    login initial

--- app/app.js
+++ app/app.js
@@ -1,24 +1,50 @@
-import {App, Platform} from 'ionic-angular';
+import {App, IonicApp, Platform, MenuController} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
-import {TabsPage} from './pages/tabs/tabs';
+import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
+import {ListPage} from './pages/list/list';
 
 
 @App({
-  template: '<ion-nav [root]="rootPage"></ion-nav>',
-  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+  templateUrl: 'build/pages/login/login.html',
+  config: {
+    mode: 'ios'
+  } // http://ionicframework.com/docs/v2/api/config/Config/
 })
-export class MyApp {
+class MyApp {
   static get parameters() {
-    return [[Platform]];
+    return [[IonicApp], [Platform], [MenuController]];
   }
 
-  constructor(platform) {
-    this.rootPage = TabsPage;
+  constructor(app, platform, menu) {
+    // set up our app
+    this.app = app;
+    this.platform = platform;
+    this.menu = menu;
+    this.initializeApp();
 
-    platform.ready().then(() => {
+    // set our app's pages
+    this.pages = [
+      {title: 'Hello Ionic', component: HelloIonicPage},
+      {title: 'My First List', component: ListPage}
+    ];
+
+    // make HelloIonicPage the root (or first) page
+    this.rootPage = HelloIonicPage;
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
     });
+  }
+
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    let nav = this.app.getComponent('nav');
+    nav.setRoot(page.component);
   }
 }
